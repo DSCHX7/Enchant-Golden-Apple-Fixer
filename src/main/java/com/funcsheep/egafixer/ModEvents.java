5ea6b925a72152cb2e6c7e0e37b6b12509a95b44
DSCHX7 <DSCHX7@outlook.com>
2025-10-07 09:55:25 +0800

    Upadte Forge 1.17.1

--- src/main/java/com/funcsheep/egafixer/ModEvents.java
+++ src/main/java/com/funcsheep/egafixer/ModEvents.java
@@ -1,11 +1,11 @@
 package com.funcsheep.egafixer;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,25 +20,24 @@ public class ModEvents {
 
         // 检查是否是附魔金苹果
         if (itemStack.getItem() == Items.ENCHANTED_GOLDEN_APPLE) {
-            if (entity instanceof PlayerEntity) {
-                PlayerEntity player = (PlayerEntity) entity;
+            if (entity instanceof Player) {
+                Player player = (Player) entity;
 
                 // 清除原有的效果
-                player.removePotionEffect(Effects.REGENERATION);
-                player.removePotionEffect(Effects.ABSORPTION);
-                player.removePotionEffect(Effects.FIRE_RESISTANCE);
-                player.removePotionEffect(Effects.RESISTANCE);
+                player.removeEffect(MobEffects.REGENERATION);
+                player.removeEffect(MobEffects.ABSORPTION);
+                player.removeEffect(MobEffects.FIRE_RESISTANCE);
+                player.removeEffect(MobEffects.DAMAGE_RESISTANCE);
 
                 // 添加 1.8 版本的效果
                 // 恢复生命值 V (Regeneration V - 30秒)
-                player.addPotionEffect(new EffectInstance(Effects.REGENERATION, 600, 4));
+                player.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 600, 4));
                 // 伤害吸收 IV (Absorption IV - 2分钟)
-                player.addPotionEffect(new EffectInstance(Effects.ABSORPTION, 2400, 3));
+                player.addEffect(new MobEffectInstance(MobEffects.ABSORPTION, 2400, 3));
                 // 抗火 (Fire Resistance - 5分钟)
-                player.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 6000, 0));
-                // 抗性提升 V (Resistance V - 5分钟)
-                player.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 6000, 1));
-
+                player.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 6000, 0));
+                // 抗性提升 I (Resistance I - 5分钟)
+                player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 6000, 0));
             }
         }
     }
